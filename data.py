670754667016306edb1226f5ef752eef37746ed0
Shorya1835 <117476655+Shorya1835@users.noreply.github.com>
2026-03-31 23:46:42 +0530

    Update data.py

--- data.py
+++ data.py
@@ -2,6 +2,28 @@ import time
 import torch
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from PIL import Image
+
+class IndexedMNIST(datasets.MNIST):
+    def __getitem__(self, index):
+        """
+        Overrides the default __getitem__ to also return the index.
+        This is critical for TDDS to save the scores for the correct sample.
+        """
+        img, target = self.data[index], int(self.targets[index])
+
+        # Convert to PIL Image to be compatible with standard torchvision transforms
+        img = Image.fromarray(img.numpy(), mode='L')
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        # RETURN INDEX ALONG WITH IMG AND TARGET
+        return img, target, index
 
 ########################################################################################################################
 # Load Data
@@ -30,6 +52,25 @@ def load_dataset(args):
         train_loader, test_loader = load_cifar10(args)
     elif args.dataset == 'cifar100':
         train_loader, test_loader = load_cifar100(args)
+    elif args.dataset == 'mnist':
+        num_classes = 10
+        
+        transform_train = transforms.Compose([
+            transforms.Resize((32, 32)),
+            transforms.Grayscale(num_output_channels=3), 
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.1307, 0.1307, 0.1307), std=(0.3081, 0.3081, 0.3081)),
+        ])
+        
+        transform_test = transforms.Compose([
+            transforms.Resize((32, 32)),
+            transforms.Grayscale(num_output_channels=3),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.1307, 0.1307, 0.1307), std=(0.3081, 0.3081, 0.3081)),
+        ])
+    
+        train_dataset = IndexedMNIST(root=args.data_path, train=True, download=True, transform=transform_train)
+        test_dataset = datasets.MNIST(root=args.data_path, train=False, download=True, transform=transform_test)
     else:
         raise NotImplementedError("Dataset not supported: {}".format(args.dataset))
     return train_loader, test_loader
