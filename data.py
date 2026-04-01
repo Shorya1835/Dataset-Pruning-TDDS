d03e6c29029855850cede153a31528c88b9737bd
Abhishek Baghel <122452418+abhishekbaghel11@users.noreply.github.com>
2026-04-01 20:04:53 +0530

    Update data.py

--- data.py
+++ data.py
@@ -5,6 +5,58 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from PIL import Image
 
+import torchvision
+import numpy as np
+
+class IMBALANCECIFAR10(torchvision.datasets.CIFAR10):
+    cls_num = 10
+
+    def __init__(self, root, imb_type='exp', imb_factor=0.01, rand_number=0, train=True,
+                 transform=None, target_transform=None, download=False):
+        super(IMBALANCECIFAR10, self).__init__(root, train, transform, target_transform, download)
+        np.random.seed(rand_number)
+        img_num_list = self.get_img_num_per_cls(self.cls_num, imb_type, imb_factor)
+        self.gen_imbalanced_data(img_num_list)
+
+    def get_img_num_per_cls(self, cls_num, imb_type, imb_factor):
+        img_max = len(self.data) / cls_num
+        img_num_per_cls = []
+        if imb_type == 'exp':
+            for cls_idx in range(cls_num):
+                num = img_max * (imb_factor ** (cls_idx / (cls_num - 1.0)))
+                img_num_per_cls.append(int(num))
+        elif imb_type == 'step':
+            for cls_idx in range(cls_num // 2):
+                img_num_per_cls.append(int(img_max))
+            for cls_idx in range(cls_num // 2):
+                img_num_per_cls.append(int(img_max * imb_factor))
+        else:
+            img_num_per_cls.extend([int(img_max)] * cls_num)
+        return img_num_per_cls
+
+    def gen_imbalanced_data(self, img_num_per_cls):
+        new_data = []
+        new_targets = []
+        targets_np = np.array(self.targets, dtype=np.int64)
+        classes = np.unique(targets_np)
+        self.num_per_cls_dict = dict()
+        for the_class, the_img_num in zip(classes, img_num_per_cls):
+            self.num_per_cls_dict[the_class] = the_img_num
+            idx = np.where(targets_np == the_class)[0]
+            np.random.shuffle(idx)
+            selec_idx = idx[:the_img_num]
+            new_data.append(self.data[selec_idx, ...])
+            new_targets.extend([the_class, ] * the_img_num)
+        new_data = np.vstack(new_data)
+        self.data = new_data
+        self.targets = new_targets
+
+    def get_cls_num_list(self):
+        cls_num_list = []
+        for i in range(self.cls_num):
+            cls_num_list.append(self.num_per_cls_dict[i])
+        return cls_num_list
+
 class IndexedMNIST(datasets.MNIST):
     def __getitem__(self, index):
         # Extract image and target
@@ -96,7 +148,11 @@ def load_cifar10(args):
         transforms.Normalize(mean, std)
     ])
     
-    train_data = dset.CIFAR10(args.data_path, train=True, transform=train_transform, download=True)
+    train_data = IMBALANCECIFAR10(
+        root=args.data_path, imb_type='exp', imb_factor=0.05, rand_number=42,
+        train=True, download=True, transform=train_transform
+    )
+    
     target_index = [[train_data.targets[i], i] for i in range(len(train_data.targets))]
     train_data.targets = target_index
     
